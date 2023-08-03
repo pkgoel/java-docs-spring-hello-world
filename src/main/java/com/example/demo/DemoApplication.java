f1ce60a4c3c03089eeb97a7f239f63f945aaab37
pkgoel <pulkitgoel07@gmail.com>
2023-08-03 21:34:45 +0000

    db connection fixed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -23,95 +23,65 @@ import java.io.PrintWriter;
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
 	String CLIENT_ID = "347123406383-vpn5n3bkm8itcce6sbp3ambjvdq0rm21.apps.googleusercontent.com";
-	String DB_CONNECTION_STRING =
-	"jdbc:sqlserver://beepmeupdbsqlserver.database.windows.net:1433;"
-			+ "database=beepmeupdb;"
-			+ "user=beepmeup@beepmeupdbsqlserver;"
-			+ "password=Nishant1-db;"
-			+ "encrypt=true;"
-			+ "trustServerCertificate=false;"
-			+ "loginTimeout=30;";
-				String url = "jdbc:sqlserver://beepmeupdbsqlserver.database.windows.net:1433;database=beepmeupdb;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-	String username = "beepmeup@beepmeupdbsqlserver";
-	String password = "Nishant1-db";
+	String DB_CONNECTION_STRING = "jdbc:sqlserver://beepmeupdbsqlserver.database.windows.net:1433;database=beepmeupdb;user=beepmeup@beepmeupdbsqlserver;password=Nishant1-db;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.GET,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	String healthCheck() {
 		return JSONObject.quote("Health Check : Good");
 	}
 
-	@RequestMapping(value = "/account", method = RequestMethod.GET,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/account", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	String fetchAccountDetails() {
 		return JSONObject.quote("accountkey: {account details response as json}");
 	}
 
-	@RequestMapping(value = "/getAlertMode", method = RequestMethod.GET,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/getAlertMode", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	String getAlertMode(String userId) {
-		
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-
-			System.out.print("Connecting to SQL Server ... ");
-			try (Connection connection = DriverManager.getConnection(DB_CONNECTION_STRING))        {
-				return JSONObject.quote("sql querying successful");
-			}
-		} catch (Exception e) {
-			System.out.println();
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			e.printStackTrace(pw);
-			String sStackTrace = sw.toString();
-			return JSONObject.quote("sql querying failed" + sStackTrace);
-			
-		}
-		/*
-		try {
-			Connection conn = DriverManager.getConnection(url, username, password);
-			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("select isActive from dbo.alert_mode_status where userid = " + userId);
+			Connection connection = DriverManager.getConnection(DB_CONNECTION_STRING);
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement
+					.executeQuery("select isActive from dbo.alert_mode_status where userid = " + userId);
 			return JSONObject.quote("alertStatus: " + resultSet.toString());
-		} 
-		catch(Exception e) {return JSONObject.quote("sql querying failed" + e.getMessage());
+		} catch (Exception e) {
+			return JSONObject.quote("sql querying failed" + e.getMessage());
 		}
-		*/
 	}
 
-	@RequestMapping(value = "/setAlertMode", method = RequestMethod.GET,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/setAlertMode", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	String setAlertMode(String userId, Boolean setActive) {
 		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			String activeStatus = setActive ? ", 1 )" : ", 0 )";
 			Connection conn = DriverManager.getConnection(DB_CONNECTION_STRING);
 			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("insert into dbo.alert_mode_status (userid, isActive) values (" + userId + activeStatus);
+			ResultSet resultSet = statement.executeQuery(
+					"insert into dbo.alert_mode_status (userid, isActive) values (" + userId + activeStatus);
 			return JSONObject.quote("Edit status: " + resultSet.toString());
-		} 
-		catch(Exception e) {return JSONObject.quote("sql querying failed" + e.getMessage());
+		} catch (Exception e) {
+			return JSONObject.quote("sql querying failed" + e.getMessage());
 		}
 	}
 
-	@RequestMapping(value = "/googleauth", method = RequestMethod.GET,
-                produces = MediaType.APPLICATION_JSON_VALUE)
-	String googleAuth(String idTokenString) {		
+	@RequestMapping(value = "/googleauth", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	String googleAuth(String idTokenString) {
 		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-		.setAudience(Collections.singletonList(CLIENT_ID))
-		.build();
-		try {GoogleIdToken idToken = verifier.verify(idTokenString);
+				.setAudience(Collections.singletonList(CLIENT_ID))
+				.build();
+		try {
+			GoogleIdToken idToken = verifier.verify(idTokenString);
 			if (idToken != null) {
 				Payload payload = idToken.getPayload();
-	
+
 				// Print user identifier
 				String userId = payload.getSubject();
 				System.out.println("User ID: " + userId);
-	
+
 				// Get profile information from payload
 				String email = payload.getEmail();
 				boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
@@ -121,10 +91,10 @@ public class DemoApplication extends SpringBootServletInitializer {
 				String familyName = (String) payload.get("family_name");
 				String givenName = (String) payload.get("given_name");
 			}
+		} catch (Exception e) {
+			return JSONObject.quote("accountkey: {invalid token " + idTokenString + " }" + e.getLocalizedMessage());
 		}
-		catch(Exception e) {		return JSONObject.quote("accountkey: {invalid token "+idTokenString+" }" + e.getLocalizedMessage());
-	}
-		
+
 		return JSONObject.quote("accountkey: {account details response as json}");
 	}
 }
