e248899d0e4ea4f76b8c6dcd9a5a84507b7f8fe8
pkgoel <pulkitgoel07@gmail.com>
2023-08-03 15:00:55 +0100

    fourth

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
-import org.JSONObject;
+import org.json.JSONObject;
 
 @SpringBootApplication
 @RestController
@@ -17,13 +17,13 @@ public class DemoApplication extends SpringBootServletInitializer {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/", method = RequestMethod.GET,
+	@RequestMapping(value = "/", method = RequestMethod.GET,
                 produces = MediaType.APPLICATION_JSON_VALUE)
 	String sayHello() {
 		return JSONObject.quote("Hello World");
 	}
 
-	@RequestMapping("/account", method = RequestMethod.GET,
+	@RequestMapping(value = "/account", method = RequestMethod.GET,
                 produces = MediaType.APPLICATION_JSON_VALUE)
 	String fetchAccountDetails() {
 		return JSONObject.quote("account details response");
