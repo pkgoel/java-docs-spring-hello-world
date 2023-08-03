37c5fd6e35b11c8e6d1af9483907a4687b2561de
pkgoel <pulkitgoel07@gmail.com>
2023-08-03 21:58:21 +0000

    j

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -47,6 +47,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement
 					.executeQuery("select isActive from dbo.alert_mode_status where userid = " + userId);
+					return JSONObject.quote("AlertStatus for user "+resultSet.toString());
 			String alertStatus = resultSet.getBoolean("isActive") ? "Active" : "Inactive";
 			return JSONObject.quote("AlertStatus for user is : " + alertStatus);
 		} catch (Exception e) {
